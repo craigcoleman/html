f7ba9047a9d3c7c482d3d9447af1e7003ba73032
craigcoleman <coleman@owt.com>
2015-11-10 13:00:19 +0000

    first push

---
+++ index.php
@@ -0,0 +1,56 @@
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <title>111001.cc</title>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="stylesheet" href="bs/dist/css/bootstrap.min.css">
+    <link rel="stylesheet" href="bs/dist/css/custom.css">
+  <script src="bs/js/jquery.min.js"></script>
+  <script src="bs/js/bootstrap.min.js"></script>
+</head>
+<body>
+
+  <ul class="nav nav-pills nav-justified">
+    <li class="active"><a href="http://111001.cc">111001.cc</a></li>
+    <li><a href="http://52.27.10.197" target ="_blank">ec2</a></li>
+      <li><a href="http://iuomo.github.io/">@iuomo</a></li>
+   <li><a href="logs/">logs</a></li>
+	 <li><a href="android/" target = "_blank">andriod</a></li>
+    <li><a href="gentoo/" target = "_blank">gentoo</a></li>
+  </ul>
+</div>
+<pre>
+
+AWS
+https://forums.aws.amazon.com
+https://console.aws.amazon.com/iam
+https://www.youtube.com/watch?v=T64qFcyTGAU
+ec2 52.27.10.197
+
+ssh login
+
+ssh -i "file.pem" ec2-user@127.0.0.1
+
+127.0.0.1 is your ip of your AWS 
+
+
+green : 2C5E4F
+yellow: FFB612
+
+blue: #002b5c
+gold: #b18d03
+
+pushing web page with git
+cwc@ip-172-31-37-69 ~ $ git config --global user.name "githubuername"
+cwc@ip-172-31-37-69 ~ $ git config --global user.email "githubemail@gmail.com"
+cwc@ip-172-31-37-69 ~ $ git config --list
+user.name=githubusername
+user.email=githubusername@gmail.com
+cwc@ip-172-31-37-69 ~ $ git clone https://github.com/craigcoleman/html.git  (this is public)
+
+</pre>
+</body>
+</html>
+
